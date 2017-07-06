2c806b4ebef1e32c7e11803415e05f246d7c014d
Waldo van der Merwe <wvdm@emss.co.za>
2017-07-06 16:32:35 +0200

    This is the first commit. This will either create a new repository on my personal github account or it might add to the current repository named "Testy" which is already created.

---
+++ tests/tests.R
@@ -0,0 +1,4 @@
+#I need to contrcut a test. Can it be multiple lines of "expect_that..." or must it be contained within a "test_that"
+#function, I do not know. When will these be called and by who, I do not know either. Can functions be tested that have
+#not been exported?
+expect_that(make_filename(2013), equals("accident_2013.csv.bz2"))
