3d33fa535956a31b5583c6aca887d96a514db126
supervan2 <waldovdm@gmail.com>
2017-07-09 19:59:36 +0200

    Changes is how to import packages.

--- tests/tests.R
+++ tests/tests.R
@@ -1,4 +1,10 @@
 #I need to contrcut a test. Can it be multiple lines of "expect_that..." or must it be contained within a "test_that"
 #function, I do not know. When will these be called and by who, I do not know either. Can functions be tested that have
-#not been exported?
+
+#' These are the tests that should run on Travis
+#'
+#' Should this have markdown comments? No one will really ever see this.
+#'
+#'@importFrom testthat
+#'
 expect_that(make_filename(2013), equals("accident_2013.csv.bz2"))
