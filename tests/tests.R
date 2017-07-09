198fefb7c52e8bd9b71df68fd2e95807ef5aab31
supervan2 <waldovdm@gmail.com>
2017-07-09 23:27:35 +0200

    Trying to get test file to work

--- tests/tests.R
+++ tests/tests.R
@@ -1,5 +1,5 @@
 #I need to contrcut a test. Can it be multiple lines of "expect_that..." or must it be contained within a "test_that"
 #function, I do not know. When will these be called and by who, I do not know either. Can functions be tested that have
 
-
+library(testthat)
 testthat::expect_that(make_filename(2013), equals("accident_2013.csv.bz2"))
